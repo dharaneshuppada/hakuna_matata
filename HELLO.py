1ea21570ef76fdeeeb08792e047b8347b83dfbe8
dharaneshuppada <dharaneshuppada@gmail.com>
2024-09-03 20:47:40 +0530

    Update HELLO.py

--- HELLO.py
+++ HELLO.py
@@ -5,8 +5,7 @@ st.title("F1")
 
 df = pd.read_csv(r"C:\Users\uppad\Desktop\sample_project-1\Car Sales Dataset - Car details v3.csv")
 
-col = st.sidebar.multiselect("Select any column", 
-							df.columns) 
+col = st.sidebar.multiselect("Select any column", df.columns) 
 
 prg = st.progress(0) 
 
